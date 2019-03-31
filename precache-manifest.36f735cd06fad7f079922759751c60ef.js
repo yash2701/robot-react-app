d6551485108196d3c1399e4cdc3a121375b6a9cd
YASH THUMAR <yash27095@gmail.com>
2019-03-31 14:23:11 -0400

    Updates

---
+++ precache-manifest.36f735cd06fad7f079922759751c60ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robot-react-app/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "bece33b3d3104f010ce6",
+    "url": "/robot-react-app/static/js/runtime~main.8ae7f6bb.js"
+  },
+  {
+    "revision": "a7832d3bf5d38d75c26e",
+    "url": "/robot-react-app/static/js/main.26902366.chunk.js"
+  },
+  {
+    "revision": "29eeb2c8b6a9867b5d03",
+    "url": "/robot-react-app/static/js/2.7cb68033.chunk.js"
+  },
+  {
+    "revision": "a7832d3bf5d38d75c26e",
+    "url": "/robot-react-app/static/css/main.fcfcec52.chunk.css"
+  },
+  {
+    "revision": "29eeb2c8b6a9867b5d03",
+    "url": "/robot-react-app/static/css/2.ff079b17.chunk.css"
+  },
+  {
+    "revision": "984834373e002510cfef2185671c191a",
+    "url": "/robot-react-app/index.html"
+  }
+];
